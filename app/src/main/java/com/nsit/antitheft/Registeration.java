42ea9614eaf2a50f8478bc421355f500111605a0
Sanjay Kumar <sanjay150898kumar@gmail.com>
2018-10-26 23:48:56 +0530

    Minor changes

--- app/src/main/java/com/nsit/antitheft/Registeration.java
+++ app/src/main/java/com/nsit/antitheft/Registeration.java
@@ -1,6 +1,8 @@
 package com.nsit.antitheft;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -35,6 +37,8 @@ public class Registeration extends AppCompatActivity {
     private TextView loginTextView;
     private String licenseCardURL = null;
     private String adhaarCardURL = null;
+    private static int ADHAAR_CARD = 1;
+    private static int LICENESE_CARD = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +51,15 @@ public class Registeration extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         dobEditText = findViewById(R.id.dobEditText);
         phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
+
         drivingLicenseCardView = findViewById(R.id.drivingLicenseCardView);
         drivingLicenseStatusTextView = findViewById(R.id.drivingLicenseStatusTextView);
         drivingLicenseImageView = findViewById(R.id.drivingLicenseImageView);
+
         adhaarCardView = findViewById(R.id.adhaarCardView);
         adhaarCardStatusTextView = findViewById(R.id.adhaarCardStatusTextView);
         adhaarCardImageView = findViewById(R.id.adhaarCardImageView);
+
         houseNoEditText = findViewById(R.id.houseNoEditText);
         localityEditText = findViewById(R.id.localityEditText);
         cityEditText = findViewById(R.id.cityEditText);
@@ -64,6 +71,28 @@ public class Registeration extends AppCompatActivity {
 
 
         drivingLicenseCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                drivingLicenseStatusTextView  drivingLicenseImageView
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, LICENESE_CARD);
+
+            }
+        });
+
+        adhaarCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                adhaarCardStatusTextView  adhaarCardImageView
+
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, ADHAAR_CARD);
+
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String username = usernameEditText.getText().toString();
@@ -85,22 +114,6 @@ public class Registeration extends AppCompatActivity {
                 else{
                     Toast.makeText(Registeration.this,"Adhaar or License missing!",Toast.LENGTH_LONG).show();
                 }
-
-//                drivingLicenseStatusTextView  drivingLicenseImageView
-            }
-        });
-
-        adhaarCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                adhaarCardStatusTextView  adhaarCardImageView
-            }
-        });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
             }
         });
 
@@ -111,6 +124,21 @@ public class Registeration extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ADHAAR_CARD) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            adhaarCardImageView.setImageBitmap(image);
+            adhaarCardStatusTextView.setText("STATUS : ACCEPTED");
+            adhaarCardStatusTextView.setTextColor(Color.parseColor("#ff0000"));
+        }
+        if (requestCode == LICENESE_CARD) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            drivingLicenseImageView.setImageBitmap(image);
+            drivingLicenseStatusTextView.setText("STATUS : ACCEPTED");
+            drivingLicenseStatusTextView.setTextColor(Color.parseColor("#ff0000"));
+        }
+    }
+
 }
